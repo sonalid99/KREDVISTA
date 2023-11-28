b3ecba92e1668a60759fe65bb003fed5f6eb316b
ASUS <ASUS@DASHLAPTOP>
2023-11-28 17:03:18 +0530

    rebase

---
+++ src/test/java/java/com/testcsriptPractice/Demo4Test2.java
@@ -0,0 +1,14 @@
+package java.com.testcsriptPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo4Test2 {
+	@Test(groups = "smoke")
+	public void demo3() {
+		System.out.println("====Demo2====");
+	}
+public void demo7() {
+	System.out.println("hi hello bye");
+
+	}
+}
---
+++ src/test/java/java/com/testcsriptPractice/RebaseClassTest.java
@@ -0,0 +1,14 @@
+package java.com.testcsriptPractice;
+
+import org.testng.annotations.Test;
+
+public class RebaseClassTest {
+	@Test(groups = "smoke")
+	public void demo3() {
+		System.out.println("====Demo2====");
+	}
+public void demo7() {
+	System.out.println("hi hello bye======");
+
+	}
+}
