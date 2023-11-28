22c32691b8cf51275300c0099368bc82cbac7549
ashirbad121 <ashirbad.sonu123@gmail.com>
2023-11-28 16:13:59 +0530

    ok

---
+++ src/test/java/java/com/testcsriptPractice/TestCase1.java
@@ -0,0 +1,17 @@
+package java.com.testcsriptPractice;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+
+	@Test(groups = "smoke")
+	public void demo4() {
+		System.out.println("====Demo4====");
+
+	}
+	@Test(groups = "smoke")
+	public void demo5() {
+		System.out.println("====Demo5====");
+
+	}
+}
